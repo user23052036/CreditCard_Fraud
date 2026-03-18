f8e8ec556644394af62fe99bb59d8d93bfcf9b6e
Yash Chugh <yashchugh112@gmail.com>
2026-03-18 07:27:02 +0000

    added data model

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import numpy as np
 
+from pydantic import BaseModel
+from typing import List
+
+class InputData(BaseModel):
+    features: List[float]
+
 app = FastAPI()
 
 app.add_middleware(
@@ -17,7 +23,7 @@ model = joblib.load("../models/xgboost_model.pkl")
 print(model)
 
 @app.post("/predict")
-def predict(data: dict):
-    features = np.array(data["features"]).reshape(1, -1)
+def predict(data: InputData):
+    features = np.array(data.features).reshape(1, -1)
     prob = model.predict_proba(features)[0][1]
     return {"probability": float(prob)}
